23dbedc0706d11182a11a06d4779ddfcb374a880
sonquer <sonquer@o2.pl>
2020-01-21 22:31:11 +0100

    Added solution for 8 & 9

--- 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
+++ 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
@@ -3,4 +3,39 @@ document.addEventListener("DOMContentLoaded", function () {
     Poniżej napisz kod rozwiązujący zadania. Odpowiedzi oddzielaj komentarzami.
      */
 
+    //Zadanie 0
+    let title = document.getElementsByClassName("title");
+
+    function getDataAnimation(element) {
+        let dataAnimation = element[0].dataset.animation;
+        return dataAnimation;
+    }
+
+    console.log(getDataAnimation(title));
+
+    //Zadanie 1
+
+    const home1 = document.querySelector('#home');
+    const home2 = document.getElementById('home');
+
+    const firstLi = document.querySelector('[data-direction]');
+    const firstBlock = document.querySelector('block');
+
+    console.log({ home1, home2, firstLi, firstBlock });
+
+    //Zadanie 2
+    const nav = document.querySelector('nav li');
+    console.log(nav);
+
+    const divs = document.querySelectorAll('div p');
+    console.log(divs);
+
+    const divsInArticle = document.querySelector('article div');
+    console.log(divsInArticle);
+
+    //Zadanie 3
+    const articleTag = document.querySelector('article.first');
+    const everyH1 = articleTag.querySelectorAll('h1').length;
+    console.log(); // uzupełnic
+
 });
--- 1_Zadania/obowiązkowe/9_DOM_Element/js/app.js
+++ 1_Zadania/obowiązkowe/9_DOM_Element/js/app.js
@@ -5,8 +5,68 @@ document.addEventListener("DOMContentLoaded", function () {
     var blocks = document.querySelectorAll(".block");
     var links = document.querySelector(".links").children;
 
-    /*
-    Poniżej napisz kod rozwiązujący zadania
-     */
+    //Zadanie 0
+    var getDatasinfo = function (elements) {
+        var tab = [];
+        for (var i = 0; i < elements.length; i++) {
+            tab.push(elements[i].dataset.color);
+        }
+
+        return tab;
+    }
+    console.log(getDatasinfo(links));
+
+    //Zadanie 1
+    console.log('home element:', homeElement);
+    console.log('child element: ', childElements);
+    console.log('baner element: ', banner);
+    console.log('blocks element: ', blocks);
+    console.log('links element: ', links);
+
+    for (var i = 0; i < childElements[i].length; i++) {
+        console.log('nazwa tagu: ', childElements[i].tagName)
+        console.log('nazwa klasy: ', childElements[i].classList)
+    };
+
+    for (var i = 0; i < blocks[i].length; i++) {
+        console.log('nazwa tagu: ', blocks[i].tagName)
+        console.log('nazwa klasy: ', blocks[i].classList)
+    };
+
+    for (var i = 0; i < links[i].length; i++) {
+        console.log('nazwa tagu: ', links[i].tagName)
+        console.log('nazwa klasy: ', links[i].classList)
+    };
+
+    //Zadanie 3
+
+    var newBlocks = document.querySelector('.blocks');
+    console.log(newBlocks.innerHTML);
+    console.log(newBlocks.outerHTML);
+    //dopisać komentarz
+
+    newBlocks.innerHTML = "Nowa wartosc diva o klasie blocks";
+
+    var mainFooter = document.querySelector('#mainFooter');
+    console.log(mainFooter);
+
+    function getId(element) {
+        var id = element.getAttribute('id');
+        console.log(id);
+        return id;
+    }
+    getId(mainFooter);
+
+    //Zadanie 4
+    function getTags(elements) {
+        var tab = [];
+
+        for (var i = 0; i < elements[i].length; i++) {
+            tab.push(elements[i].tagName);
+        }
+        return tab;
+    }
+    console.log(getTags(childElements));
+
 
 });
